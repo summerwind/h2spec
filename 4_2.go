53c7b2a565f64c2adfa90ee2b890fe6d552e78bb
Moto Ishizawa <summerwind.jp@gmail.com>
2015-01-22 00:46:02 +0900

    Add test about the size of the frame that exceeds the SETTINGS_MAX_FRAME_SIZE

--- 4_2.go
+++ 4_2.go
@@ -1,8 +1,10 @@
 package h2spec
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/bradfitz/http2"
+	"github.com/bradfitz/http2/hpack"
 	"time"
 )
 
@@ -49,6 +51,51 @@ func TestFrameSize(ctx *Context) {
 		defer http2Conn.conn.Close()
 
 		fmt.Fprintf(http2Conn.conn, "\x00\x00\x0f\x06\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
+
+	loop:
+		for {
+			f, err := http2Conn.ReadFrame(3 * time.Second)
+			if err != nil {
+				break loop
+			}
+			switch f := f.(type) {
+			case *http2.GoAwayFrame:
+				if f.ErrCode == http2.ErrCodeFrameSize {
+					result = true
+					break loop
+				}
+			}
+		}
+
+		PrintResult(result, desc, msg, 0)
+	}(ctx)
+
+	func(ctx *Context) {
+		desc := "Sends large size frame that exceeds the SETTINGS_MAX_FRAME_SIZE"
+		result := false
+
+		http2Conn := CreateHttp2Conn(ctx, false)
+		defer http2Conn.conn.Close()
+
+		var buf bytes.Buffer
+		hdrs := []hpack.HeaderField{
+			pair(":method", "GET"),
+			pair(":scheme", "http"),
+			pair(":path", "/"),
+			pair(":authority", ctx.Authority()),
+		}
+		enc := hpack.NewEncoder(&buf)
+		for _, hf := range hdrs {
+			_ = enc.WriteField(hf)
+		}
+
+		var hp http2.HeadersFrameParam
+		hp.StreamID = 1
+		hp.EndStream = false
+		hp.EndHeaders = true
+		hp.BlockFragment = buf.Bytes()
+		http2Conn.fr.WriteHeaders(hp)
+		http2Conn.fr.WriteData(1, true, []byte(GetDummyData(16385)))
 
 	loop:
 		for {
