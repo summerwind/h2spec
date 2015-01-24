b8455697d0a4b0dee41a719d9bec3291c842d774
Moto Ishizawa <summerwind.jp@gmail.com>
2015-01-24 16:04:21 +0900

    Remove invalid tests

--- 4_2.go
+++ 4_2.go
@@ -16,60 +16,6 @@ func TestFrameSize(ctx *Context) {
 	msg := "The endpoint MUST send a FRAME_SIZE_ERROR error."
 
 	func(ctx *Context) {
-		desc := "Sends too small size frame"
-		result := false
-
-		http2Conn := CreateHttp2Conn(ctx, true)
-		defer http2Conn.conn.Close()
-
-		fmt.Fprintf(http2Conn.conn, "\x00\x00\x00\x08\x00\x00\x00\x00\x00")
-
-	loop:
-		for {
-			f, err := http2Conn.ReadFrame(ctx.Timeout)
-			if err != nil {
-				break loop
-			}
-			switch f := f.(type) {
-			case *http2.GoAwayFrame:
-				if f.ErrCode == http2.ErrCodeFrameSize {
-					result = true
-					break loop
-				}
-			}
-		}
-
-		PrintResult(result, desc, msg, 0)
-	}(ctx)
-
-	func(ctx *Context) {
-		desc := "Sends too large size frame"
-		result := false
-
-		http2Conn := CreateHttp2Conn(ctx, true)
-		defer http2Conn.conn.Close()
-
-		fmt.Fprintf(http2Conn.conn, "\x00\x00\x0f\x06\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
-
-	loop:
-		for {
-			f, err := http2Conn.ReadFrame(ctx.Timeout)
-			if err != nil {
-				break loop
-			}
-			switch f := f.(type) {
-			case *http2.GoAwayFrame:
-				if f.ErrCode == http2.ErrCodeFrameSize {
-					result = true
-					break loop
-				}
-			}
-		}
-
-		PrintResult(result, desc, msg, 0)
-	}(ctx)
-
-	func(ctx *Context) {
 		desc := "Sends large size frame that exceeds the SETTINGS_MAX_FRAME_SIZE"
 		result := false
 
