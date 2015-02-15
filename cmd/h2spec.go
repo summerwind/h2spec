0542a16054dba05198e139f1c8bef93f2eac043c
Moto Ishizawa <summerwind.jp@gmail.com>
2015-02-15 23:34:16 +0900

    Change default timeout to 2 sec

--- cmd/h2spec.go
+++ cmd/h2spec.go
@@ -26,7 +26,7 @@ func main() {
 	host := flag.String("h", "127.0.0.1", "Target host")
 	useTls := flag.Bool("t", false, "Connect over TLS")
 	insecureSkipVerify := flag.Bool("k", false, "Don't verify server's certificate")
-	timeout := flag.Int("o", 3, "Maximum time allowed for test.")
+	timeout := flag.Int("o", 2, "Maximum time allowed for test.")
 
 	var sectionFlag sections
 	flag.Var(&sectionFlag, "s", "Section number on which to run the test")
@@ -38,7 +38,7 @@ func main() {
 		fmt.Println("  -h:     Target host. (Default: 127.0.0.1)")
 		fmt.Println("  -t:     Connect over TLS. (Default: false)")
 		fmt.Println("  -k:     Don't verify server's certificate. (Default: false)")
-		fmt.Println("  -o:     Maximum time allowed for test. (Default: 3)")
+		fmt.Println("  -o:     Maximum time allowed for test. (Default: 2)")
 		fmt.Println("  -s:     Section number on which to run the test. (Example: -s 6.1 -s 6.2)")
 		fmt.Println("  --help: Display this help and exit.")
 		os.Exit(1)
