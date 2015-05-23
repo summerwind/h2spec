d83f7d8d058eb6836a088625c2121f7aa359b400
Moto Ishizawa <summerwind.jp@gmail.com>
2015-05-23 23:19:28 +0900

    Add --version option

--- cmd/h2spec.go
+++ cmd/h2spec.go
@@ -10,6 +10,8 @@ import (
 	"time"
 )
 
+const VERSION = "v0.0.9"
+
 type sections []string
 
 func (s *sections) String() string {
@@ -22,11 +24,12 @@ func (s *sections) Set(v string) error {
 }
 
 func main() {
-	port := flag.Int("p", 0, "Target port")
-	host := flag.String("h", "127.0.0.1", "Target host")
-	useTls := flag.Bool("t", false, "Connect over TLS")
-	insecureSkipVerify := flag.Bool("k", false, "Don't verify server's certificate")
+	port := flag.Int("p", 0, "Target port.")
+	host := flag.String("h", "127.0.0.1", "Target host.")
+	useTls := flag.Bool("t", false, "Connect over TLS.")
+	insecureSkipVerify := flag.Bool("k", false, "Don't verify server's certificate.")
 	timeout := flag.Int("o", 2, "Maximum time allowed for test.")
+	version := flag.Bool("version", false, "Display version information and exit.")
 
 	var sectionFlag sections
 	flag.Var(&sectionFlag, "s", "Section number on which to run the test")
@@ -34,17 +37,24 @@ func main() {
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS]\n\n", os.Args[0])
 		fmt.Println("Options:")
-		fmt.Println("  -p:     Target port. (Default: 80 or 443)")
-		fmt.Println("  -h:     Target host. (Default: 127.0.0.1)")
-		fmt.Println("  -t:     Connect over TLS. (Default: false)")
-		fmt.Println("  -k:     Don't verify server's certificate. (Default: false)")
-		fmt.Println("  -o:     Maximum time allowed for test. (Default: 2)")
-		fmt.Println("  -s:     Section number on which to run the test. (Example: -s 6.1 -s 6.2)")
-		fmt.Println("  --help: Display this help and exit.")
+		fmt.Println("  -p:        Target port. (Default: 80 or 443)")
+		fmt.Println("  -h:        Target host. (Default: 127.0.0.1)")
+		fmt.Println("  -t:        Connect over TLS. (Default: false)")
+		fmt.Println("  -k:        Don't verify server's certificate. (Default: false)")
+		fmt.Println("  -o:        Maximum time allowed for test. (Default: 2)")
+		fmt.Println("  -s:        Section number on which to run the test. (Example: -s 6.1 -s 6.2)")
+		fmt.Println("  --version: Display version information and exit.")
+		fmt.Println("  --help:    Display this help and exit.")
 		os.Exit(1)
 	}
 
 	flag.Parse()
+
+	if *version {
+		fmt.Fprintf(os.Stderr, "h2spec %s\n", VERSION)
+		os.Exit(0)
+	}
+
 	if *port == 0 {
 		if *useTls {
 			*port = 443
