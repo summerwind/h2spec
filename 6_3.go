f0a9c53ea824703bf47876ec59084fda21f72ffe
Frederik Deweerdt <fdeweerdt@fastly.com>
2016-12-08 09:04:24 -0800

    Add a test to cover the case where a PRIORITY frame is sent for a higher stream id than a subsequent HEADER frame

--- 6_3.go
+++ 6_3.go
@@ -2,6 +2,10 @@ package h2spec
 
 import (
 	"fmt"
+	"io"
+	"net"
+	"syscall"
+
 	"golang.org/x/net/http2"
 )
 
@@ -59,6 +63,74 @@ func PriorityTestGroup(ctx *Context) *TestGroup {
 
 			actualCodes := []http2.ErrCode{http2.ErrCodeFrameSize}
 			return TestStreamError(ctx, http2Conn, actualCodes)
+		},
+	))
+
+	tg.AddTestCase(NewTestCase(
+		"Sends a PRIORITY frame for an idle stream, then send a HEADER frame for a lower stream id",
+		"The endpoint MUST respond to the HEADER request with no connection error.",
+		func(ctx *Context) (pass bool, expected []Result, actual Result) {
+			http2Conn := CreateHttp2Conn(ctx, true)
+			defer http2Conn.conn.Close()
+
+			// PRIORITY Frame
+			fmt.Fprintf(http2Conn.conn, "\x00\x00\x05\x02\x00\x00\x00\x00\x03")
+			fmt.Fprintf(http2Conn.conn, "\x80\x00\x00\x00\x0a")
+
+			hdrs2 := commonHeaderFields(ctx)
+			hdrs2[0].Value = "HEAD"
+
+			var hp2 http2.HeadersFrameParam
+			hp2.StreamID = 1
+			hp2.EndStream = true
+			hp2.EndHeaders = true
+			hp2.BlockFragment = http2Conn.EncodeHeader(hdrs2)
+			http2Conn.fr.WriteHeaders(hp2)
+
+		loop:
+			for {
+				f, err := http2Conn.ReadFrame(ctx.Timeout)
+				if err != nil {
+					opErr, ok := err.(*net.OpError)
+					if err == io.EOF || (ok && opErr.Err == syscall.ECONNRESET) {
+						rf, ok := actual.(*ResultFrame)
+						if actual == nil || (ok && rf.Type != http2.FrameGoAway) {
+							actual = &ResultConnectionClose{}
+						}
+					} else if err == TIMEOUT {
+						if actual == nil {
+							actual = &ResultTestTimeout{}
+						}
+					} else {
+						actual = &ResultError{err}
+					}
+					break loop
+				}
+
+				switch f := f.(type) {
+				case *http2.DataFrame:
+					actual = CreateResultFrame(f)
+					if f.FrameHeader.Flags.Has(http2.FlagDataEndStream) && f.Header().Length == 0 {
+						pass = true
+						break loop
+					}
+				case *http2.HeadersFrame:
+					actual = CreateResultFrame(f)
+					if f.FrameHeader.Flags.Has(http2.FlagHeadersEndStream) {
+						pass = true
+						break loop
+					}
+				case *http2.GoAwayFrame:
+					actual = CreateResultFrame(f)
+					break loop
+				case *http2.RSTStreamFrame:
+					actual = CreateResultFrame(f)
+					break loop
+				}
+				actual = CreateResultFrame(f)
+			}
+
+			return pass, expected, actual
 		},
 	))
 
