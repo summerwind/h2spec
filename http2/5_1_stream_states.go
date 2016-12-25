d51238be4f7854cbf7089f710b8cd4dae28f04f3
Moto Ishizawa <summerwind.jp@gmail.com>
2016-12-25 18:12:42 +0900

    Add more test cases for RFC7540 5_1

--- http2/5_1_stream_states.go
+++ http2/5_1_stream_states.go
@@ -9,6 +9,10 @@ import (
 func StreamStates() *spec.TestGroup {
 	tg := NewTestGroup("5.1", "Stream States")
 
+	// idle:
+	// Receiving any frame other than HEADERS or PRIORITY on a stream
+	// in this state MUST be treated as a connection error
+	// (Section 5.4.1) of type PROTOCOL_ERROR.
 	tg.AddTestCase(&spec.TestCase{
 		Desc:        "idle: Sends a DATA frame",
 		Requirement: "The endpoint MUST treat this as a connection error of type PROTOCOL_ERROR.",
@@ -24,6 +28,10 @@ func StreamStates() *spec.TestGroup {
 		},
 	})
 
+	// idle:
+	// Receiving any frame other than HEADERS or PRIORITY on a stream
+	// in this state MUST be treated as a connection error
+	// (Section 5.4.1) of type PROTOCOL_ERROR.
 	tg.AddTestCase(&spec.TestCase{
 		Desc:        "idle: Sends a RST_STREAM frame",
 		Requirement: "The endpoint MUST treat this as a connection error of type PROTOCOL_ERROR.",
@@ -39,6 +47,10 @@ func StreamStates() *spec.TestGroup {
 		},
 	})
 
+	// idle:
+	// Receiving any frame other than HEADERS or PRIORITY on a stream
+	// in this state MUST be treated as a connection error
+	// (Section 5.4.1) of type PROTOCOL_ERROR.
 	tg.AddTestCase(&spec.TestCase{
 		Desc:        "idle: Sends a WINDOW_UPDATE frame",
 		Requirement: "The endpoint MUST treat this as a connection error of type PROTOCOL_ERROR.",
@@ -54,6 +66,10 @@ func StreamStates() *spec.TestGroup {
 		},
 	})
 
+	// idle:
+	// Receiving any frame other than HEADERS or PRIORITY on a stream
+	// in this state MUST be treated as a connection error
+	// (Section 5.4.1) of type PROTOCOL_ERROR.
 	tg.AddTestCase(&spec.TestCase{
 		Desc:        "idle: Sends a CONTINUATION frame",
 		Requirement: "The endpoint MUST treat this as a connection error of type PROTOCOL_ERROR.",
@@ -65,13 +81,17 @@ func StreamStates() *spec.TestGroup {
 
 			headers := spec.CommonHeaders(c)
 			blockFragment := conn.EncodeHeaders(headers)
-
 			conn.WriteContinuation(1, true, blockFragment)
 
 			return spec.VerifyConnectionError(conn, http2.ErrCodeProtocol)
 		},
 	})
 
+	// half-closed (remote):
+	// If an endpoint receives additional frames, other than
+	// WINDOW_UPDATE, PRIORITY, or RST_STREAM, for a stream that is in
+	// this state, it MUST respond with a stream error (Section 5.4.2)
+	// of type STREAM_CLOSED.
 	tg.AddTestCase(&spec.TestCase{
 		Desc:        "half closed (remote): Sends a DATA frame",
 		Requirement: "The endpoint MUST respond with a stream error of type STREAM_CLOSED.",
@@ -98,6 +118,11 @@ func StreamStates() *spec.TestGroup {
 		},
 	})
 
+	// half-closed (remote):
+	// If an endpoint receives additional frames, other than
+	// WINDOW_UPDATE, PRIORITY, or RST_STREAM, for a stream that is in
+	// this state, it MUST respond with a stream error (Section 5.4.2)
+	// of type STREAM_CLOSED.
 	tg.AddTestCase(&spec.TestCase{
 		Desc:        "half closed (remote): Sends a HEADERS frame",
 		Requirement: "The endpoint MUST respond with a stream error of type STREAM_CLOSED.",
@@ -131,6 +156,11 @@ func StreamStates() *spec.TestGroup {
 		},
 	})
 
+	// half-closed (remote):
+	// If an endpoint receives additional frames, other than
+	// WINDOW_UPDATE, PRIORITY, or RST_STREAM, for a stream that is in
+	// this state, it MUST respond with a stream error (Section 5.4.2)
+	// of type STREAM_CLOSED.
 	tg.AddTestCase(&spec.TestCase{
 		Desc:        "half closed (remote): Sends a CONTINUATION frame",
 		Requirement: "The endpoint MUST respond with a stream error of type STREAM_CLOSED.",
@@ -159,9 +189,161 @@ func StreamStates() *spec.TestGroup {
 		},
 	})
 
+	// closed:
+	// An endpoint MUST NOT send frames other than PRIORITY on a closed
+	// stream.
+	tg.AddTestCase(&spec.TestCase{
+		Desc:        "closed: Sends a PRIORITY frame",
+		Requirement: "The endpoint MUST treat this as a connection error of type STREAM_CLOSED.",
+		Run: func(c *config.Config, conn *spec.Conn) error {
+			var streamID uint32 = 1
+
+			err := conn.Handshake()
+			if err != nil {
+				return err
+			}
+
+			headers := spec.CommonHeaders(c)
+			hp := http2.HeadersFrameParam{
+				StreamID:      streamID,
+				EndStream:     true,
+				EndHeaders:    true,
+				BlockFragment: conn.EncodeHeaders(headers),
+			}
+			conn.WriteHeaders(hp)
+
+			err = spec.VerifyStreamClose(conn)
+			if err != nil {
+				return err
+			}
+
+			pp := http2.PriorityParam{
+				StreamDep: 0,
+				Exclusive: false,
+				Weight:    255,
+			}
+			conn.WritePriority(streamID, pp)
+
+			conn.WritePing(false, [8]byte{})
+
+			return spec.VerifyFrameType(conn, http2.FramePing)
+		},
+	})
+
+	// closed:
+	// An endpoint that receives any frame other than PRIORITY after
+	// receiving a RST_STREAM MUST treat that as a stream error
+	// (Section 5.4.2) of type STREAM_CLOSED.
+	tg.AddTestCase(&spec.TestCase{
+		Desc:        "closed: Sends a DATA frame after sending RST_STREAM frame",
+		Requirement: "The endpoint MUST treat this as a stream error of type STREAM_CLOSED.",
+		Run: func(c *config.Config, conn *spec.Conn) error {
+			var streamID uint32 = 1
+
+			err := conn.Handshake()
+			if err != nil {
+				return err
+			}
+
+			headers := spec.CommonHeaders(c)
+			hp := http2.HeadersFrameParam{
+				StreamID:      streamID,
+				EndStream:     false,
+				EndHeaders:    true,
+				BlockFragment: conn.EncodeHeaders(headers),
+			}
+			conn.WriteHeaders(hp)
+
+			conn.WriteRSTStream(streamID, http2.ErrCodeCancel)
+
+			conn.WriteData(streamID, true, []byte("test"))
+
+			return spec.VerifyStreamError(conn, http2.ErrCodeStreamClosed)
+		},
+	})
+
+	// closed:
+	// An endpoint that receives any frame other than PRIORITY after
+	// receiving a RST_STREAM MUST treat that as a stream error
+	// (Section 5.4.2) of type STREAM_CLOSED.
+	tg.AddTestCase(&spec.TestCase{
+		Desc:        "closed: Sends a HEADERS frame after sending RST_STREAM frame",
+		Requirement: "The endpoint MUST treat this as a stream error of type STREAM_CLOSED.",
+		Run: func(c *config.Config, conn *spec.Conn) error {
+			var streamID uint32 = 1
+
+			err := conn.Handshake()
+			if err != nil {
+				return err
+			}
+
+			headers := spec.CommonHeaders(c)
+			hp1 := http2.HeadersFrameParam{
+				StreamID:      streamID,
+				EndStream:     false,
+				EndHeaders:    true,
+				BlockFragment: conn.EncodeHeaders(headers),
+			}
+			conn.WriteHeaders(hp1)
+
+			conn.WriteRSTStream(streamID, http2.ErrCodeCancel)
+
+			hp2 := http2.HeadersFrameParam{
+				StreamID:      streamID,
+				EndStream:     true,
+				EndHeaders:    true,
+				BlockFragment: conn.EncodeHeaders(headers),
+			}
+			conn.WriteHeaders(hp2)
+
+			return spec.VerifyStreamError(conn, http2.ErrCodeStreamClosed)
+		},
+	})
+
+	// closed:
+	// An endpoint that receives any frame other than PRIORITY after
+	// receiving a RST_STREAM MUST treat that as a stream error
+	// (Section 5.4.2) of type STREAM_CLOSED.
+	tg.AddTestCase(&spec.TestCase{
+		Desc:        "closed: Sends a CONTINUATION frame after sending RST_STREAM frame",
+		Requirement: "The endpoint MUST treat this as a stream error of type STREAM_CLOSED.",
+		Run: func(c *config.Config, conn *spec.Conn) error {
+			var streamID uint32 = 1
+
+			err := conn.Handshake()
+			if err != nil {
+				return err
+			}
+
+			headers := spec.CommonHeaders(c)
+			hp := http2.HeadersFrameParam{
+				StreamID:      streamID,
+				EndStream:     false,
+				EndHeaders:    true,
+				BlockFragment: conn.EncodeHeaders(headers),
+			}
+			conn.WriteHeaders(hp)
+
+			conn.WriteRSTStream(streamID, http2.ErrCodeCancel)
+
+			dummyHeaders := spec.DummyHeaders(c, 1)
+			conn.WriteContinuation(streamID, true, conn.EncodeHeaders(dummyHeaders))
+
+			codes := []http2.ErrCode{
+				http2.ErrCodeStreamClosed,
+				http2.ErrCodeProtocol,
+			}
+			return spec.VerifyStreamError(conn, codes...)
+		},
+	})
+
+	// closed:
+	// An endpoint that receives any frames after receiving a frame
+	// with the END_STREAM flag set MUST treat that as a connection
+	// error (Section 6.4.1) of type STREAM_CLOSED.
 	tg.AddTestCase(&spec.TestCase{
 		Desc:        "closed: Sends a DATA frame",
-		Requirement: "The endpoint MUST treat this as a stream error of type STREAM_CLOSED.",
+		Requirement: "The endpoint MUST treat this as a connection error of type STREAM_CLOSED.",
 		Run: func(c *config.Config, conn *spec.Conn) error {
 			var streamID uint32 = 1
 
@@ -186,13 +368,17 @@ func StreamStates() *spec.TestGroup {
 
 			conn.WriteData(streamID, true, []byte("test"))
 
-			return spec.VerifyStreamError(conn, http2.ErrCodeStreamClosed)
+			return spec.VerifyConnectionError(conn, http2.ErrCodeStreamClosed)
 		},
 	})
 
+	// closed:
+	// An endpoint that receives any frames after receiving a frame
+	// with the END_STREAM flag set MUST treat that as a connection
+	// error (Section 6.4.1) of type STREAM_CLOSED.
 	tg.AddTestCase(&spec.TestCase{
 		Desc:        "closed: Sends a HEADERS frame",
-		Requirement: "The endpoint MUST treat this as a stream error of type STREAM_CLOSED.",
+		Requirement: "The endpoint MUST treat this as a connection error of type STREAM_CLOSED.",
 		Run: func(c *config.Config, conn *spec.Conn) error {
 			var streamID uint32 = 1
 
@@ -217,13 +403,17 @@ func StreamStates() *spec.TestGroup {
 
 			conn.WriteHeaders(hp)
 
-			return spec.VerifyStreamError(conn, http2.ErrCodeStreamClosed)
+			return spec.VerifyConnectionError(conn, http2.ErrCodeStreamClosed)
 		},
 	})
 
+	// closed:
+	// An endpoint that receives any frames after receiving a frame
+	// with the END_STREAM flag set MUST treat that as a connection
+	// error (Section 6.4.1) of type STREAM_CLOSED.
 	tg.AddTestCase(&spec.TestCase{
 		Desc:        "closed: Sends a CONTINUATION frame",
-		Requirement: "The endpoint MUST treat this as a stream error of type STREAM_CLOSED.",
+		Requirement: "The endpoint MUST treat this as a connection error of type STREAM_CLOSED.",
 		Run: func(c *config.Config, conn *spec.Conn) error {
 			var streamID uint32 = 1
 
@@ -252,7 +442,11 @@ func StreamStates() *spec.TestGroup {
 
 			conn.WriteContinuation(streamID, true, blockFragment[:frameSize])
 
-			return spec.VerifyStreamError(conn, http2.ErrCodeStreamClosed, http2.ErrCodeProtocol)
+			codes := []http2.ErrCode{
+				http2.ErrCodeStreamClosed,
+				http2.ErrCodeProtocol,
+			}
+			return spec.VerifyConnectionError(conn, codes...)
 		},
 	})
 
