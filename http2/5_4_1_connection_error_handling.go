3b549e7e943a19056adf2cb9b7e381ce07e55161
Moto Ishizawa <summerwind.jp@gmail.com>
2016-12-25 18:37:34 +0900

    Add ref comments for RFC7540 5.4.1

--- http2/5_4_1_connection_error_handling.go
+++ http2/5_4_1_connection_error_handling.go
@@ -12,8 +12,10 @@ import (
 func ConnectionErrorHandling() *spec.TestGroup {
 	tg := NewTestGroup("5.4.1", "Connection Error Handling")
 
+	// After sending the GOAWAY frame for an error condition,
+	// the endpoint MUST close the TCP connection.
 	tg.AddTestCase(&spec.TestCase{
-		Desc:        "Sends invalid frame for connection close",
+		Desc:        "Sends an invalid PING frame for connection close",
 		Requirement: "The endpoint MUST close the TCP connection",
 		Run: func(c *config.Config, conn *spec.Conn) error {
 			err := conn.Handshake()
@@ -29,9 +31,12 @@ func ConnectionErrorHandling() *spec.TestGroup {
 		},
 	})
 
+	// An endpoint that encounters a connection error SHOULD first send
+	// a GOAWAY frame (Section 6.8) with the stream identifier of the last
+	// stream that it successfully received from its peer.
 	tg.AddTestCase(&spec.TestCase{
 		Strict:      true,
-		Desc:        "Sends invalid frame for GOAWAY frame",
+		Desc:        "Sends an invalid PING frame to receive GOAWAY frame",
 		Requirement: "An endpoint that encounters a connection error SHOULD first send a GOAWAY frame",
 		Run: func(c *config.Config, conn *spec.Conn) error {
 			var actual spec.Event
